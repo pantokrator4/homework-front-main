e608f67e2808fe6c2b999124adf6146d6b8a8456
pantokrator4 <mralexfour@gmail.com>
2025-05-06 19:36:56 +0400

    0.12 hw10 is done

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,26 @@
 const initState = {
-    isLoading: false,
-}
+  isLoading: false,
+};
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+export const loadingReducer = (state = initState, action: LoadingActionType): any => {
+  // fix any
+  switch (action.type) {
+    // пишет студент  // need to fix
+		case "CHANGE_LOADING":
+			const newState = {...state, isLoading: action.isLoading}
+			return newState
 
-        default:
-            return state
-    }
-}
+    default:
+      return state;
+  }
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+  type: "CHANGE_LOADING";
+  isLoading: boolean;
+};
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+  type: "CHANGE_LOADING",
+  isLoading,
+});
